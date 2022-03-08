240e7c5c48072ca9d81c6ccfbd2da2d0c235ba9e
arjorb <arjorb@gmail.com>
2022-03-08 14:59:10 +0200

    added tab button and end of my chrome Lead extension

--- index.js
+++ index.js
@@ -5,14 +5,27 @@ const inputEl = document.getElementById('input-el');
 const inputBtn = document.getElementById('input-btn');
 const ulEl = document.getElementById('ul-el');
 const deleteBtn = document.getElementById('delete-btn');
+const tabBtn = document.getElementById('tab-btn');
 let leadFromLocal = JSON.parse(localStorage.getItem("myLeads"))
 
 if(leadFromLocal){
     myLeads = leadFromLocal;
     render(myLeads);
-}else{
-    console.log("There is no any lead!")
 }
+
+
+tabBtn.addEventListener('click', function(){
+
+    chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("myLeads",JSON.stringify(myLeads));
+        render(myLeads);
+    })
+    // for(let i = 0; i < tab.length; i++){
+       
+    // }
+    
+})
 
 inputBtn.addEventListener("click", function(){
    if(inputEl.value != ''){
